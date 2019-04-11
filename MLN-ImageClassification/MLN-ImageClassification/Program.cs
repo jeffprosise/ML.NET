ceeb6df166ad5afa47cf605712d45ed03979c03a
jeffprosise <jeffpro@wintellect.com>
2019-04-11 13:53:15 -0400

    Renamed one of the images folders

--- MLN-ImageClassification/MLN-ImageClassification/Program.cs
+++ MLN-ImageClassification/MLN-ImageClassification/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,8 +22,33 @@ namespace MLN_ImageClassification
         {
             var context = new MLContext(seed: 0);
 
+            // Load the training data
+            var trainingData = new List<ImageData>();
+            var files = Directory.EnumerateFiles(_hotDogTrainImagesPath);
 
+            foreach(var file in files)
+            {
+                var imageData = new ImageData
+                {
+                    ImagePath = Path.GetFullPath($"{_hotDogTrainImagesPath}\\{file}"),
+                    Label = "hotdog"
+                };
 
+                trainingData.Add(imageData);
+            }
+
+            files = Directory.EnumerateFiles(_notHotDogTrainImagesPath);
+
+            foreach (var file in files)
+            {
+                var imageData = new ImageData
+                {
+                    ImagePath = Path.GetFullPath($"{_notHotDogTrainImagesPath}\\{file}"),
+                    Label = "nothotdog"
+                };
+
+                trainingData.Add(imageData);
+            }
 
 
 
@@ -40,7 +66,23 @@ namespace MLN_ImageClassification
 
     public class ImagePrediction
     {
-        public float[] Score;
-        public string PredictedLabelValue;
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+        public float Probability { get; set; }
+    }
+
+    //public class ImagePrediction
+    //{
+    //    public float[] Score;
+    //    public string PredictedLabelValue;
+    //}
+
+    public struct InceptionSettings
+    {
+        public const int ImageHeight = 224;
+        public const int ImageWidth = 224;
+        public const float Mean = 117;
+        public const float Scale = 1;
+        public const bool ChannelsLast = true;
     }
 }
