838b6c2006808154f94b64da6d6ee48e03bfbe18
jeffprosise <jeffpro@wintellect.com>
2020-08-13 16:11:56 -0400

    Updated image-classification samples

--- MLN-ImageClassification/MLN-ImageClassification/Program.cs
+++ MLN-ImageClassification/MLN-ImageClassification/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.ML;
+using Microsoft.ML.Vision;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,94 +9,92 @@ namespace ImageClassification
 {
     class Program
     {
-        private static readonly string _hotDogTrainImagesPath = "..\\..\\..\\Data\\train\\hotdog";
-        private static readonly string _pizzaTrainImagesPath = "..\\..\\..\\Data\\train\\pizza";
-        private static readonly string _sushiTrainImagesPath = "..\\..\\..\\Data\\train\\sushi";
-        private static readonly string _testImagesPath = "..\\..\\..\\Data\\test";
-        private static readonly string _modelPath = "..\\..\\..\\Model\\tensorflow_inception_graph.pb";
+        private static readonly string _imagePath = "..\\..\\..\\Data";
         private static readonly string _savePath = "..\\..\\..\\Model\\hotdog.zip";
+        private static readonly string _predictedLabelColumnName = "PredictedLabel";
+        private static readonly string _keyColumnName = "LabelAsKey";
 
         static void Main(string[] args)
         {
             var context = new MLContext(seed: 0);
 
-            // Load the training data
-            var trainingData = new List<ImageData>();
-            LoadImageData(trainingData, Path.GetFullPath(_hotDogTrainImagesPath), "hotdog");
-            LoadImageData(trainingData, Path.GetFullPath(_pizzaTrainImagesPath), "pizza");
-            LoadImageData(trainingData, Path.GetFullPath(_sushiTrainImagesPath), "sushi");
+            // Create a DataView containing the image paths and labels
+            var input = LoadLabeledImagesFromPath(_imagePath);
+            var data = context.Data.LoadFromEnumerable(input);
+            data = context.Data.ShuffleRows(data);
 
-            var pipeline = context.Transforms.LoadImages(outputColumnName: "input", imageFolder: Path.GetFullPath(_hotDogTrainImagesPath), inputColumnName: "ImagePath")
-                .Append(context.Transforms.LoadImages(outputColumnName: "input", imageFolder: Path.GetFullPath(_pizzaTrainImagesPath), inputColumnName: "ImagePath"))
-                .Append(context.Transforms.LoadImages(outputColumnName: "input", imageFolder: Path.GetFullPath(_sushiTrainImagesPath), inputColumnName: "ImagePath"))
-                .Append(context.Transforms.ResizeImages(outputColumnName: "input", inputColumnName: "input", imageWidth: InceptionSettings.ImageWidth, imageHeight: InceptionSettings.ImageHeight))
-                .Append(context.Transforms.ExtractPixels(outputColumnName: "input", interleavePixelColors: InceptionSettings.ChannelsLast, offsetImage: InceptionSettings.Mean))
-                .Append(context.Model.LoadTensorFlowModel(_modelPath)
-                    .ScoreTensorFlowModel(outputColumnNames: new[] { "softmax2_pre_activation" }, inputColumnNames: new[] { "input" }, addBatchDimensionInput: true)
-                .Append(context.Transforms.Conversion.MapValueToKey(outputColumnName: "Key", inputColumnName: "Label"))
-                .Append(context.MulticlassClassification.Trainers.LbfgsMaximumEntropy(labelColumnName: "Key", featureColumnName: "softmax2_pre_activation"))
-                .Append(context.Transforms.Conversion.MapKeyToValue("PredictedLabel")));
+            // Load the images and convert the labels to keys to serve as categorical values
+            var images = context.Transforms.Conversion.MapValueToKey(inputColumnName: nameof(Input.Label), outputColumnName: _keyColumnName)
+                .Append(context.Transforms.LoadRawImageBytes(inputColumnName: nameof(Input.ImagePath), outputColumnName: nameof(Input.Image), imageFolder: _imagePath))
+                .Fit(data).Transform(data);
 
-            // Train the model
-            Console.WriteLine("Training the model...");
-            var data = context.Data.LoadFromEnumerable<ImageData>(trainingData); // Create IDataView from IEnumerable
-            var model = pipeline.Fit(data);
-            Console.WriteLine();
+            // Split the dataset for training and testing
+            var trainTestData = context.Data.TrainTestSplit(images, testFraction: 0.2, seed: 1);
+            var trainData = trainTestData.TrainSet;
+            var testData = trainTestData.TestSet;
 
-            // Make predictions using test images
-            var files = Directory.EnumerateFiles(Path.GetFullPath(_testImagesPath));
-            var predictor = context.Model.CreatePredictionEngine<ImageData, ImagePrediction>(model);
-
-            foreach (var file in files)
+            // Create an image-classification pipeline and train the model
+            var options = new ImageClassificationTrainer.Options()
             {
-                var image = new ImageData { ImagePath = file };
-                var result = predictor.Predict(image);
-                var label = result.PredictedLabel;
-                var probability = result.Score.Max();
-                Console.WriteLine($"{Path.GetFileName(file)} - {label} ({probability:P2})");
-            }
+                FeatureColumnName = nameof(Input.Image),
+                LabelColumnName = _keyColumnName,
+                ValidationSet = testData,
+                Arch = ImageClassificationTrainer.Architecture.ResnetV2101, // Pretrained DNN
+                MetricsCallback = (metrics) => Console.WriteLine(metrics),
+                TestOnTrainSet = false
+            };
+
+            var pipeline = context.MulticlassClassification.Trainers.ImageClassification(options)
+                .Append(context.Transforms.Conversion.MapKeyToValue(_predictedLabelColumnName));
+
+            Console.WriteLine("Training the model...");
+            var model = pipeline.Fit(trainData);
+
+            // Evaluate the model and show the results
+            var predictions = model.Transform(testData);
+            var metrics = context.MulticlassClassification.Evaluate(predictions, labelColumnName: _keyColumnName, predictedLabelColumnName: _predictedLabelColumnName);
+
+            Console.WriteLine();
+            Console.WriteLine($"Macro accuracy = {metrics.MacroAccuracy:P2}");
+            Console.WriteLine($"Micro accuracy = {metrics.MicroAccuracy:P2}");
+            Console.WriteLine(metrics.ConfusionMatrix.GetFormattedConfusionTable());
+            Console.WriteLine();
 
             // Save the model
             Console.WriteLine();
-            Console.WriteLine("Saving the model");
-            context.Model.Save(model, data.Schema, _savePath);
+            Console.WriteLine("Saving the model...");
+            context.Model.Save(model, trainData.Schema, _savePath);
         }
 
-        private static void LoadImageData(List<ImageData> images, string path, string label)
+        private static List<Input> LoadLabeledImagesFromPath(string path)
         {
-            var files = Directory.EnumerateFiles(path);
+            var images = new List<Input>();
+            var directories = Directory.EnumerateDirectories(path);
 
-            foreach (var file in files)
+            foreach(var directory in directories)
             {
-                var imageData = new ImageData
-                {
-                    ImagePath = file,
-                    Label = label
-                };
+                var files = Directory.EnumerateFiles(directory);
 
-                images.Add(imageData);
+                images.AddRange(files.Select(x => new Input {
+                    ImagePath = Path.GetFullPath(x),
+                    Label = Path.GetFileName(directory)
+                }));
             }
+
+            return images;
         }
     }
 
-    public class ImageData
+    public class Input
     {
+        public byte[] Image;
         public string ImagePath;
         public string Label;
     }
 
-    public class ImagePrediction
+    public class Output
     {
         public float[] Score;
         public string PredictedLabel;
-    }
-
-    public struct InceptionSettings
-    {
-        public const int ImageHeight = 224;
-        public const int ImageWidth = 224;
-        public const float Mean = 117;
-        public const float Scale = 1;
-        public const bool ChannelsLast = true;
     }
 }
